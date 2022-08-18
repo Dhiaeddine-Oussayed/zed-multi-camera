c6ca2ecd015d4839893d6382d4ab4873842babe5
dhiaeddine-oussayed <dhia.oussayed@gmail.com>
2022-08-18 14:23:41 +0200

    changes to custom_multi_zed

--- python/custom_multi_camera.py
+++ python/custom_multi_camera.py
@@ -4,7 +4,6 @@ import cv2
 from objdet.accelerated import YoloAccInference
 
 
-
 params = {
             'conf_threshold': 0.3,
             'img_size': 640,
@@ -24,46 +23,37 @@ detection_instance = YoloAccInference(params)
 zed_list = []
 left_list = []
 depth_list = []
-timestamp_list = []
-thread_list = []
-stop_signal = False
+name_list = []
 key = ''
-number_of_cameras = 4
+number_of_cameras = 3
 
 
 
 init = sl.InitParameters()
 init.camera_resolution = sl.RESOLUTION.HD720
 init.camera_fps = 30 
-
-name_list = []
-last_ts_list = []
 cameras = sl.Camera.get_device_list()
-index = 0
+runtime = sl.RuntimeParameters()
 
-for cam in cameras:
+
+for i, cam in enumerate(cameras):
 	init.set_from_serial_number(cam.serial_number)
 	name_list.append("ZED {}".format(cam.serial_number))
-	print("Opening {}".format(name_list[index]))
+	print("Opening {}".format(name_list[i]))
 	zed_list.append(sl.Camera())
 	left_list.append(sl.Mat())
 	depth_list.append(sl.Mat())
-	timestamp_list.append(0)
-	last_ts_list.append(0)
-	status = zed_list[index].open(init)
-	index = index + 1
+	zed_list[i].open(init)
 
 
-	
-runtime = sl.RuntimeParameters()
-index = index -1 
+
 while key != 113:
 	for index in range(number_of_cameras):
 		err = zed_list[index].grab(runtime)
 		if err == sl.ERROR_CODE.SUCCESS:
 			zed_list[index].retrieve_image(left_list[index], sl.VIEW.LEFT)
 			zed_list[index].retrieve_measure(depth_list[index], sl.MEASURE.DEPTH)
-			timestamp_list[index] = zed_list[index].get_timestamp(sl.TIME_REFERENCE.CURRENT).data_ns
+
 			time.sleep(0.001) #1ms
 		inference_frame = cv2.cvtColor(left_list[index].get_data(), cv2.COLOR_BGRA2BGR)
 		det_objs = detection_instance.detect(inference_frame)
@@ -74,6 +64,12 @@ while key != 113:
 		cv2.imshow(name_list[index], debug_frame)
 	key = cv2.waitKey(10)
 cv2.destroyAllWindows()
+for index in range(number_of_cameras):
+	zed_list[index].close()
 
-zed_list[index].close()
+
 print("Finish")
+
+
+
+
